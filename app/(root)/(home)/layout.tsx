0b10d89c98fd8e2552f4ee9a5218054147a78d7a
vikassinghtomar <142648634+vikassinghtomar@users.noreply.github.com>
2025-01-17 11:18:21 +0530

    Update layout.tsx

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 
 export const metadata: Metadata = {
-  title: "Coders room",
+  title: "Coder's room",
   description: 'A workspace for your team, powered by Stream Chat and Clerk.',
 };
 
